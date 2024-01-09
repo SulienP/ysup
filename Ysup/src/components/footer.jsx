c82fd08bcca9599f2d7d5204751ca6ca03a9f19e
steven04458 <steven.spein2@gmail.com>
2024-01-09 22:47:10 +0100

    creation composant footer

---
+++ Ysup/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/footer.css";
+
+const FooterPage = () => {
+    return (
+        <div className="footer">
+            <p>Établissements d’enseignement supérieur technique privé hors contrat — informations non contractuelles</p>
+        </div>
+    )
+};
+
+export default FooterPage;
--- Ysup/src/pages/home/home.jsx
+++ Ysup/src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 
+import FooterPage from "../../components/footer";
 import HeaderPage from "../../components/haeder";
 
 const HomePage = () => {
     return (
         <div >
             <HeaderPage/>
+            <FooterPage/>
         </div>
     )
 };
