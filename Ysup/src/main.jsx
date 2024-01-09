47db122f0c56e7bd282af986748e52f361ed1d4e
ckevin <kevin.chaffaux@ynov.com>
2024-01-09 15:42:00 +0100

    :fire: | delete files from ysup folder

--- Ysup/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Ways from './config/config.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Ways />
-  </React.StrictMode>,
-)
