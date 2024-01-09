ec48c3fbcbbe2d2fd22c08993c2f339d42d12ed8
ckevin <kevin.chaffaux@ynov.com>
2024-01-09 13:50:02 +0100

    :sparkles: | add login form / :lipstick: | stylized it

--- Ysup/src/config/config.jsx
+++ Ysup/src/config/config.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErrorPage from "../pages/error/error";
 import HomePage from "../pages/home/home";
+import LoginPage from "../pages/login/login_page";
 
 const Ways = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />}></Route>
-                <Route path="*" element={<ErrorPage />}></Route>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="*" element={<ErrorPage />}/>
+                <Route path="/connexion" element={<LoginPage/>}/>
             </Routes>
         </BrowserRouter>
     )
