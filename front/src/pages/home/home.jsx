7b410db10e60b7b856e04e78e885a62cb5280733
SulienP <112190974+SulienP@users.noreply.github.com>
2024-01-10 13:23:53 +0100

    merge proble

--- front/src/pages/home/home.jsx
+++ front/src/pages/home/home.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+import { getCookie } from "../../services/jwt_services";
+import axios from "axios";
+import { apiUrl } from "../../utils/constants";
+
 const HomePage = () => {
+    const [permissions, setPermissions] = useState("");
+    const [errorMsg, SetErrorMsg] = useState("");
+    const [cookieJwt, setCookieJwt] = useState("");
+    
+    useEffect(() => {
+        setCookieJwt(getCookie());
+        const getPerm = async (cookieJwt) => {
+            await axios.post(apiUrl + 'isjwtvalid', { "jwt": cookieJwt }).then((response) => {
+                setPermissions(response.data.permission);
+            }).catch((err) => {
+                SetErrorMsg(err);
+            })
+        }
+        getPerm(cookieJwt);
+    }, [cookieJwt]);
+
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <p>{permissions}</p>
+        </div>
     )
 };
 
