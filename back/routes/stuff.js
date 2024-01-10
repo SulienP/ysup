75e78fb1dbd46cfc52fa400e3253bd94cfab8f6d
ckevin <kevin.chaffaux@ynov.com>
2024-01-10 14:51:43 +0100

    :construction: | working on create ticket

--- back/routes/stuff.js
+++ back/routes/stuff.js
@@ -6,5 +6,6 @@ const stuffCtrl = require('../controllers/stuff');
 router.get('/hello',stuffCtrl.hello);
 router.post('/isuservalid',stuffCtrl.isUserValid);
 router.post('/isjwtvalid',stuffCtrl.isJwtValid);
+router.get('/getalltags',stuffCtrl.GetAllTags);
 
 module.exports = router;
