5a4f4cef555efd8ccff532b902635065cc1557c8
steven04458 <steven.spein2@gmail.com>
2024-01-12 10:15:37 +0100

    update page error

--- front/src/pages/error/error.jsx
+++ front/src/pages/error/error.jsx
@@ -1,6 +1,14 @@
+import './error.css';
+import '../../styles/global.css';
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <h1>Error</h1>
+        <div className='error alignCenter columnContainer'>
+            <div className="container_login_page_background"></div>
+            <h1>Error 404:</h1>
+            <Link to={'/'} className='button-error'>Redirection</Link>
+        </div>
     )
 };
 
